a773f8caa9388c902a46c82793e8b2ccd567166c
Antoine Toulme <antoine@lunar-ocean.com>
2018-08-13 17:23:02 -0700

    Enforce formatting code again

--- lib/eth_common_test/harness.ex
+++ lib/eth_common_test/harness.ex
@@ -11,24 +11,27 @@ defmodule EthCommonTest.Harness do
   end
 
   defmacro eth_test(test_set, test_subset_or_subsets, tests, fun) do
-    test_subsets = case test_subset_or_subsets do
-      test_subsets when is_list(test_subsets) -> test_subsets
-      test_subset -> [test_subset]
-    end
+    test_subsets =
+      case test_subset_or_subsets do
+        test_subsets when is_list(test_subsets) -> test_subsets
+        test_subset -> [test_subset]
+      end
 
     for test_subset <- test_subsets do
       for {test_name, test} <- EthCommonTest.Helpers.read_test_file(test_set, test_subset),
-        ( tests == :all or Enum.member?(tests, String.to_atom(test_name)) ) do
-          json = Poison.encode!(test)
+          tests == :all or Enum.member?(tests, String.to_atom(test_name)) do
+        json = Poison.encode!(test)
 
-          quote do
-            test("#{unquote(test_set)} - #{unquote(test_subset)} - #{unquote(test_name)}", test_params) do
-              test = unquote(json) |> Poison.decode!
-              unquote(fun).(test, unquote(test_subset), unquote(test_name), test_params)
-            end
+        quote do
+          test(
+            "#{unquote(test_set)} - #{unquote(test_subset)} - #{unquote(test_name)}",
+            test_params
+          ) do
+            test = unquote(json) |> Poison.decode!()
+            unquote(fun).(test, unquote(test_subset), unquote(test_name), test_params)
           end
+        end
       end
     end
   end
-
 end
